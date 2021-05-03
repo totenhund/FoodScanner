62d315d68e8a51b8fd6fec65e65ee6b9087de01a
totenhund <caligoyan@gmail.com>
2021-05-04 00:59:33 +0300

    Qr Scanner is ready

---
+++ app/src/main/java/totenhund/com/foodscanner/start/StartFragment.kt
@@ -0,0 +1,74 @@
+package totenhund.com.foodscanner.start
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import totenhund.com.foodscanner.R
+import totenhund.com.foodscanner.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentStartBinding
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_start,
+            container,
+            false
+        )
+
+        binding.scanButton.setOnClickListener {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        }
+
+
+        return binding.root
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_PERMISSION_CODE -> {
+
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    val action = StartFragmentDirections.actionStartFragmentToScannerFragment()
+                    findNavController(this).navigate(action)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    TODO("explain user why it is vital to give permission for camera")
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
+}
